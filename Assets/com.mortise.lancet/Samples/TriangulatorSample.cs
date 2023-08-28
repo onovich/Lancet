67d609fa92e5d4947f49ec1228ed4fe3bae1bbc7
onovich <hongjianan@4399inc.com>
2023-08-28 20:34:30 +0800

    <mics> modify: Sample 优化可视化控制

--- Assets/com.mortise.lancet/Samples/TriangulatorSample.cs
+++ Assets/com.mortise.lancet/Samples/TriangulatorSample.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 namespace MortiseFrame.Lancet.Sample {
 
     public class TriangulatorSample : MonoBehaviour {
-        public Vector3[] vertices; // 定义多边形的顶点
+        Vector3[] vertices; // 定义多边形的顶点
+        public Vector3[] vertices_temp; // 临时顶点
         public List<int> indexList; // 定义多边形的顶点索引
+
+        [ContextMenu("清空")]
+        void Clear() {
+            vertices = null;
+            indexList = null;
+        }
 
         [ContextMenu("切")]
         void Clip() {
 
-            if (vertices == null || vertices.Length < 3) {
+            if (vertices_temp == null || vertices_temp.Length < 3) {
                 return;
             }
+
+            vertices = vertices_temp;
 
             // 使用Triangulator类进行三角剖分
             indexList = Triangulator.Triangulate(vertices);
 
         }
 
-        void OnDrawGizmos() {
+        void DrawPolygon_Temp() {
+            if (vertices_temp == null || vertices_temp.Length < 3) {
+                return;
+            }
 
-            if (vertices == null || vertices.Length < 3 || indexList == null || indexList.Count < 3) {
+            // 绘制多边形边界
+            Gizmos.color = Color.white;
+            for (int i = 0; i < vertices_temp.Length; i++) {
+                Gizmos.DrawLine(vertices_temp[i], vertices_temp[(i + 1) % vertices_temp.Length]);
+            }
+        }
+
+        void DrawPolygon() {
+            if (vertices == null || vertices.Length < 3) {
                 return;
             }
 
@@ -30,6 +50,12 @@ namespace MortiseFrame.Lancet.Sample {
             Gizmos.color = Color.blue;
             for (int i = 0; i < vertices.Length; i++) {
                 Gizmos.DrawLine(vertices[i], vertices[(i + 1) % vertices.Length]);
+            }
+        }
+
+        void DrawGizmozTriangulator() {
+            if (vertices == null || vertices.Length < 3 || indexList == null || indexList.Count < 3) {
+                return;
             }
 
             // 使用获得的索引绘制三角形
@@ -39,6 +65,13 @@ namespace MortiseFrame.Lancet.Sample {
                 Gizmos.DrawLine(vertices[indexList[i + 1]], vertices[indexList[i + 2]]);
                 Gizmos.DrawLine(vertices[indexList[i + 2]], vertices[indexList[i]]);
             }
+        }
+
+        void OnDrawGizmos() {
+
+            DrawPolygon_Temp();
+            DrawPolygon();
+            DrawGizmozTriangulator();
 
         }
 
